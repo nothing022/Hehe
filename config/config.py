bff3787aca9f380772df9367bd71b70455c9422b
Nᴏᴛʜɪɴɢ... <125535875+nothing022@users.noreply.github.com>
2024-11-13 01:02:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -90,7 +90,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/yourtoofan/MOMMYMUSIC",
+    "https://github.com/nothing022/Hehe",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
